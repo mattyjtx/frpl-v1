0c45c62dc78acd919fc7f1d0d353be172e4e82a2
Matthew Gonzales <mattgonzales3@yahoo.com>
2023-03-12 16:11:56 -0500

    added FollowersCard/FollowersData

---
+++ client/src/Data/FollowersData.js
@@ -0,0 +1,12 @@
+import img1 from "../img/img1.jpeg";
+import img2 from "../img/img2.jpg";
+import img3 from "../img/img3.JPG";
+import img4 from "../img/img4.png";
+
+export const Followers = [
+    {name: "frapp de la frapp", username: "fancyfrappy", img: img1 },
+    {name: "natural frapp", username: "gardenboi", img: img2 },
+    {name: "frappo chan", username: "kung-fu-frapp", img: img3 },
+    {name: "david busser", username: "iluvbigfrapp", img: img4 },
+
+];
